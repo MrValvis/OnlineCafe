8d6445e4966f4e67f708848712d55a291208cff8
Philip Valvis <valvisphilip@gmail.com>
2020-04-15 12:22:13 +0300

    added: controller basic config and webapi formatter settings

---
+++ OnlineCafe/Controllers/MenusController.cs
@@ -0,0 +1,21 @@
+﻿using OnlineCafe.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineCafe.Controllers
+{
+    public class MenusController : ApiController
+    {
+        CafeApiContext cafeapicontext = new CafeApiContext();
+
+        [HttpGet]
+        public IHttpActionResult Menus() {
+            var menus = cafeapicontext.Menus;
+            return Ok(menus);
+        }
+    }
+}
