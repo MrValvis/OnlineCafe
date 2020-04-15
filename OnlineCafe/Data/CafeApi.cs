d1ae876798c0c6edb34dedfbaec4bf7a2fd25bf2
Philip Valvis <valvisphilip@gmail.com>
2020-04-15 12:05:33 +0300

    added: sql db and connection to db

---
+++ OnlineCafe/Data/CafeApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using OnlineCafe.Models;
+
+namespace OnlineCafe.Data
+{
+    public class CafeApiContext:DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+    }
+}
